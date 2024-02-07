e417e119d80859c7aa654870125460b08879dc23
ang-rui-yan <angruiyan@gmail.com>
2024-02-07 18:13:52 +0800

    added initialise session for bot in scripts

--- scripts/initialise/main.py
+++ scripts/initialise/main.py
@@ -8,7 +8,12 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-app = Client("chats_todo_bot")
+
+API_ID = os.environ.get("API_ID")
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+app = Client("chats_todo_bot", API_ID, API_HASH, BOT_TOKEN)
 
 
 @app.on_message(filters.text & filters.private)
