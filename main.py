33629d88c133aac4f30943f326d612e3d0a84a15
ang-rui-yan <angruiyan@gmail.com>
2024-02-11 16:03:52 +0800

    added the chat separated summary

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import json
 import asyncio
 from pyrogram import Client, filters
 from bot.commands import summary, todo, event, feedback, schedule, group
+from bot import chat_handler
 from bot.commands.commands import COMMANDS, set_commands
 
 from api.openai_manager import OpenAiHelper
@@ -42,40 +43,20 @@ async def handle_help(client, message):
     await message.reply_text(reply)
 
 
+app.on_message(filters.command("summary") &
+               filters.private)(summary.handle_summary)
+app.on_message(filters.command("summary") &
+               filters.group)(summary.handle_summary_for_a_group)
+
+app.on_message(filters.group & filters.text)(
+    chat_handler.track_user_interaction)
 app.on_message(filters.command("groups"))(group.handle_manage_groups)
 app.on_message(filters.text & filters.regex(
     "^(⬅️ Previous|Next ➡️|Add Groups|Help|Exit)$"))(group.handle_group_navigation)
 app.on_message(filters.text & ~filters.regex(
     "^(⬅️ Previous|Next ➡️|Add Groups|Help|Exit)$"))(group.handle_individual_group_actions)
-# app.on_callback_query(filters.regex("^groups_add$"))(group.handle_add_group)
-# app.on_callback_query(filters.regex("^groups_view$"))(group.handle_view_group)
-# app.on_callback_query(filters.regex("^groups_delete$")
-#                       )(group.handle_delete_group)
-
-
-@app.on_message(filters.command("all"))
-async def handle_do_all_actions(client, message):
-    command = message.command[0]
-    reply = COMMANDS[command]["message"]
-    error_message = COMMANDS[command]["error"]
-    null_message = COMMANDS[command]["null"]
-    current_chat_id = f"{message.chat.id}"
-
-    try:
-        if current_chat_id in user_messages:
-            await message.reply_text(reply)
-            await message.reply_text(str(user_messages[f"{message.chat.id}"]))
-        else:
-            await message.reply_text(null_message)
-    except:
-        await message.reply_text(error_message)
-
 
 app.on_message(filters.command("start"))(handle_start)
-
-app.on_message(filters.command("summary"))(summary.handle_summary)
-app.on_callback_query(filters.regex(r"^summary_"))(
-    summary.handle_summary_selection)
 
 
 app.on_message(filters.command("todo"))(todo.handle_todo)
@@ -92,17 +73,35 @@ app.on_message(filters.command("feedback"))(feedback.handle_feedback)
 app.on_message(filters.command("schedule"))(schedule.handle_schedule)
 
 
-# testing if the bot can read messages
-@app.on_message(filters.group & filters.command("test") & filters.text)
-async def echo(client, message):
-    print(IS_OPENAI_TURN_ON)
-    if IS_OPENAI_TURN_ON:
-        # response = openai_helper.get_response(message.text)
-        # await message.reply_text("off")
-        print("ai on")
-    else:
-        # await message.reply_text(f"you said {message.text}")
-        print("ai off")
+# @app.on_message(filters.command("all"))
+# async def handle_do_all_actions(client, message):
+#     command = message.command[0]
+#     reply = COMMANDS[command]["message"]
+#     error_message = COMMANDS[command]["error"]
+#     null_message = COMMANDS[command]["null"]
+#     current_chat_id = f"{message.chat.id}"
+
+#     try:
+#         if current_chat_id in user_messages:
+#             await message.reply_text(reply)
+#             await message.reply_text(str(user_messages[f"{message.chat.id}"]))
+#         else:
+#             await message.reply_text(null_message)
+#     except:
+#         await message.reply_text(error_message)
+
+
+# # testing if the bot can read messages
+# @app.on_message(filters.group & filters.command("test") & filters.text)
+# async def echo(client, message):
+#     print(IS_OPENAI_TURN_ON)
+#     if IS_OPENAI_TURN_ON:
+#         # response = openai_helper.get_response(message.text)
+#         # await message.reply_text("off")
+#         print("ai on")
+#     else:
+#         # await message.reply_text(f"you said {message.text}")
+#         print("ai off")
 
 
 async def main():
