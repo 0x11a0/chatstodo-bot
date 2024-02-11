e4d52e71baccfb06710b8b114e253bf1b2b8230e
ang-rui-yan <angruiyan@gmail.com>
2024-02-11 21:01:50 +0800

    added summariser

--- initialise_session.sh
+++ initialise_session.sh
@@ -5,4 +5,9 @@
 # Load and export .env variables
 export $(grep -v '^#' .env | xargs)
 
-printf '%s\ny\n' "$BOT_TOKEN" | python ./scripts/initialise/main.py | mv chats_todo_bot.session ../../
+{
+    echo "$BOT_TOKEN"
+    echo "y"
+} | python ./scripts/initialise/main.py
+
+mv ./scripts/initialise/chats_todo_bot.session ../../
